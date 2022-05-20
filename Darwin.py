b01a52113b71d0446d0e18587b83dfcf732c8626
AHKIL <76881997+AHKIL@users.noreply.github.com>
2022-05-20 19:16:49 +0530

    removed streamlit watermarks

--- Darwin.py
+++ Darwin.py
@@ -11,7 +11,7 @@ import socket
 
 st.set_page_config(layout="wide",page_title="Darwin")
 hide_st_style = """<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;} header {visibility: hidden;}</style>"""
-
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 firebase_cred=dict(st.secrets)
 firebase_cred.pop('web')
